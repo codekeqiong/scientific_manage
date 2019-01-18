f55a18e97c8bc43f42de302f30a02a2d273e28a6
june_zkq <zhoukeqiong@jisuapp.cn>
2019-01-18 18:50:20 +0800

    完善个人信息

--- src/page/personal_manage/perfect_info.vue
+++ src/page/personal_manage/perfect_info.vue
@@ -2,7 +2,13 @@
   <div class="perfect_info">
     <div class="info-title">个人中心</div>
     <div class="info-content">
-      <el-form :model="rulesForm" :rules="rules" ref="rulesForm" label-width="100px" class="rulesForm">
+      <el-form
+        :model="rulesForm"
+        :rules="rules"
+        ref="rulesForm"
+        label-width="100px"
+        class="rulesForm"
+      >
         <el-form-item label="账号" prop="account">
           <el-input v-model="rulesForm.account"></el-input>
         </el-form-item>
@@ -16,16 +22,26 @@
           </el-radio-group>
         </el-form-item>
         <el-form-item label="院系" prop="second_college">
-          <el-select v-model="ruleForm.second_college" placeholder="请选择院系">
-            <el-option v-for="(item,index) in collegeArr" :key="index" label="item.label" value="item.value"></el-option>
+          <el-select v-model="rulesForm.second_college" placeholder="请选择院系">
+            <el-option
+              v-for="(item,index) in collegeArr"
+              :key="index"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="籍贯" prop="native_place">
           <el-input v-model="rulesForm.native_place"></el-input>
         </el-form-item>
-         <el-form-item label="学历" prop="education">
-          <el-select v-model="ruleForm.education" placeholder="请选择学历">
-            <el-option v-for="(item,index) in educationArr" :key="index" label="item.label" value="item.value"></el-option>
+        <el-form-item label="学历" prop="education">
+          <el-select v-model="rulesForm.education" placeholder="请选择学历">
+            <el-option
+              v-for="(item,index) in educationArr"
+              :key="index"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="出生年月" prop="birth_place">
@@ -34,81 +50,106 @@
         <el-form-item label="联系电话" prop="phone">
           <el-input type="number" v-model="rulesForm.phone"></el-input>
         </el-form-item>
+        <el-form-item> 
+          <el-button>取消</el-button>
+          <el-button type="primary" @click="onSubmit">更新</el-button>
+        </el-form-item>
       </el-form>
     </div>
   </div>
 </template>
 <script>
 export default {
-  data(){
-    return{
-      rulesForm:{
-        account: '',
-        account_name:'',
-        sex: '',
-        second_college:'',
-        native_place:'',
-        education: '',
-        birth_place: '',
-        phone: ''
+  data() {
+    return {
+      rulesForm: {
+        account: "",
+        account_name: "",
+        sex: "",
+        second_college: "",
+        native_place: "",
+        education: "",
+        birth_place: "",
+        phone: ""
       },
-      //  collegeArr:[{label: '应用id', value: 'apply_id'},
-      //   {label: '小程序id', value: 'program_id'},
-      //   {label: '模块名称', value: 'model_name'},
-      //   {label: '制作用户名', value: 'make_name'},
-      //   {label: '分类', value: 'kinds'},
-      //   {label: '封面', value: 'cover'},
-      //   {label: '权重', value: 'weight'},
-      //   {label: '上架状态', value: 'shelf_state'},
-      //   {label: '创建时间', value: 'create_time'}],
-      rules:{
-        account: [
-          { required: true, message: '请输入账号', trigger: 'blur' }
-        ],
+      collegeArr: [
+        { label: "数学与计算机学院", value: "数计" },
+        { label: "土木与建筑工程学院", value: "土建" },
+        { label: "人文社科学院", value: "人文" },
+        { label: "经济与管理学院", value: "经管" },
+        { label: "艺术学院", value: "艺术" },
+        { label: "外国语学院", value: "外国语" },
+        { label: "钒钛学院", value: "钒钛" },
+        { label: "生物与化学工程学院", value: "生化" },
+        { label: "电气信息工程学院", value: "电气" },
+        { label: "医学院", value: "医学" },
+        { label: "机械工程学院", value: "机械" },
+        { label: "交通与汽车工程学院", value: "交汽" },
+        { label: "康养学院", value: "康养" },
+        { label: "智能制造学院", value: "智能" }
+      ],
+      educationArr: [
+        { label: "小学", value: "小学" },
+        { label: "初中", value: "初中" },
+        { label: "中技", value: "中技" },
+        { label: "高中", value: "高中" },
+        { label: "中专", value: "中专" },
+        { label: "大专", value: "大专" },
+        { label: "本科", value: "本科" },
+        { label: "硕士", value: "硕士" },
+        { label: "MBA", value: "MBA" },
+        { label: "EMBA", value: "EMBA" },
+        { label: "博士", value: "博士" },
+        { label: "其他", value: "其他" }
+      ],
+      rules: {
+        account: [{ required: true, message: "请输入账号", trigger: "blur" }],
         account_name: [
-          { required: true, message: '请输入姓名', trigger: 'blur' }
+          { required: true, message: "请输入姓名", trigger: "blur" }
         ],
-        sex: [
-          { required: true, message: '请选择性别', trigger: 'blur' }
-        ],
+        sex: [{ required: true, message: "请选择性别", trigger: "blur" }],
         second_college: [
-          { required: true, message: '请输选择二级学院', trigger: 'blur' }
+          { required: true, message: "请输选择二级学院", trigger: "blur" }
         ],
         native_place: [
-          { required: true, message: '请输入籍贯', trigger: 'blur' }
+          { required: true, message: "请输入籍贯", trigger: "blur" }
         ],
-        education: [
-          { required: true, message: '请选择学历', trigger: 'blur' }
+        education: [{ required: true, message: "请选择学历", trigger: "blur" }],
+        birth_place: [
+          { required: true, message: "请输入出生年月", trigger: "blur" }
         ],
-        birth_place: [       
-          { required: true, message:'请输入出生年月', trigger: 'blur' }
-        ],
-        phone: [       
-          { required: true, message:'请输入联系方式', trigger: 'blur' }
-        ]
-      },
-    }
+        phone: [{ required: true, message: "请输入联系方式", trigger: "blur" }]
+      }
+    };
   }
-}
+};
 </script>
 <style scoped>
-.perfect_info{
+.perfect_info {
   width: 100%;
   height: 850px;
   background-color: #fff;
 }
-.info-title{
+.info-title {
   width: 100%;
   height: 60px;
   line-height: 60px;
-  margin-left:20px;
+  margin-left: 20px;
   font-size: 22px;
   text-align: left;
   border-bottom: 1px solid #eee;
   position: relative;
 }
-.info-content{
+.info-content {
+  /* border: 1px solid #409EFF; */
   margin-top: 30px;
   width: 800px;
+  padding-right: 50px;
+  padding-top: 30px;
+  box-sizing: border-box;
+}
+.el-input--suffix .el-input__inner{
+  padding-right: 30px;
+  width: 649px;
 }
 </style>
